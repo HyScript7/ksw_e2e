241802f1af3d8b479dbba6d23d039e40577147ad
HyScript7 <hyscript7@seznam.cz>
2026-03-10 08:06:11 +0100

    Unfuck configuration

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -125,7 +125,7 @@ export const config: WebdriverIO.Config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec','jsonhtml'],
+    reporters: ['spec'],
 
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
